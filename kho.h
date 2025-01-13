502d3600246e112e7b3c0ee5a4b02f8c38800389
medect06 <minh.na2414934@sis.hust.edu.vn>
2025-01-14 06:48:47 +0700

    Update kho.h

--- kho.h
+++ kho.h
@@ -5,22 +5,21 @@ using namespace std;
 
 class NguyenLieu {
 public:
-    char ten[DO_DAI_TEN];
-    char donVi[DO_DAI_DONVI];
+    string ten;
+    string donVi;
     float soLuong, gia;
-    void chuanHoa();                          //Cài đặt mặc định cho nguyên liệu, tránh giá trị rác
-    void saoChep(const NguyenLieu &nguyenLieuNguon);        //Sao chép thông tin từ nguyên liệu nguồn
+    
+    void chuanHoa(); //Cài đặt mặc định cho nguyên liệu, tránh giá trị rác
+    void saoChep(const NguyenLieu &nguyenLieuNguon); //Sao chép thông tin từ nguyên liệu nguồn
+
     static void themNhieu(vector<NguyenLieu>& danhSach, const string &duongDan); //Thêm nhiều nguyên liệu vào file
-    static int kiemTraSo(const string &duongDan);                       //Kiểm tra số lượng nguyên liệu trong file 
-    static void docFile(const string &duongDan, vector<NguyenLieu>& kho);  //Đọc dữ liệu từ file
+    static int kiemTraSo(const string &duongDan); //Kiểm tra số lượng nguyên liệu trong file 
+    static void docFile(const string &duongDan, vector<NguyenLieu>& kho); //Đọc dữ liệu từ file
     static void xoaNguyenLieu(vector<NguyenLieu>& kho, const NguyenLieu& nlXoa); //Xóa nguyên liệu khỏi file
 };
 
 // Chức năng liên quan tới vector<NguyenLieu>
-void themNguyenLieu(vector<NguyenLieu>& danhSach, 
-                   const NguyenLieu& nguyenLieuMoi);   //Thêm nguyên liệu vào danh sách
-
-void hienThiKho(const vector<NguyenLieu>& danhSach);             //Hiển thị danh sách nguyên liệu trong kho
-void layNguyenLieu(vector<NguyenLieu>& danhSach, 
-                   const NguyenLieu& nlSuDung, int soMon); //Lấy nguyên liệu từ kho để sử dụng
-void timKiemNguyenLieu(const vector<NguyenLieu>& danhSach);       //Tìm kiếm nguyên liệu trong kho
+void themNguyenLieu(vector<NguyenLieu>& danhSach, const NguyenLieu& nguyenLieuMoi); //Thêm nguyên liệu vào danh sách
+void hienThiKho(const vector<NguyenLieu>& danhSach); //Hiển thị danh sách nguyên liệu trong kho
+void layNguyenLieu(vector<NguyenLieu>& danhSach, const NguyenLieu& nlSuDung, int soMon); //Lấy nguyên liệu từ kho để sử dụng
+void timKiemNguyenLieu(const vector<NguyenLieu>& danhSach); //Tìm kiếm nguyên liệu trong kho
